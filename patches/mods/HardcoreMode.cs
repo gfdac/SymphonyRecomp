b8fea8398657bfde8877158f56b3df908c6a4929
Gustavo Clemente <guh.clemente@gmail.com>
2026-09-01 11:57:26 -0300

    Fix Stages.Current reference in HardcoreMode

--- patches/mods/HardcoreMode.cs
+++ patches/mods/HardcoreMode.cs
@@ -73,7 +73,7 @@ public static class HardcoreMode
         }
 
         // Permadeath Check (Hardcore Mode)
-        if (IsHardcore && curHp <= 0 && !_permadeathTriggered && StageManager.CurrentStage != Stage.Prologue)
+        if (IsHardcore && curHp <= 0 && !_permadeathTriggered && Stages.Current != Stage.Prologue)
         {
             _permadeathTriggered = true;
             TriggerPermadeath(m);
